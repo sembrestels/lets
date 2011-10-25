1cd0ec14c35cceabf41e66cab54e89ec848f2a97
Sem <sembrestels@riseup.net>
2011-10-25 19:37:19 +0200

    No relationship more.

--- classes/ElggLETSUser.php
+++ classes/ElggLETSUser.php
@@ -7,34 +7,21 @@ class ElggLETSUser extends ElggUser {
 	public function createAccount($container_guid){
 		$group = new ElggLETSGroup($container_guid);
 		if($group->isLETS() && $group->isMember($this->guid)){
-			if(add_entity_relationship($this->guid, 'accept_currency', $group->guid)){
-				$account = new ElggLETSAccount($this->guid, $group->guid);
-				return $account->save();
-			}
+			$account = new ElggLETSAccount($this->guid, $group->guid);
+			$account->disabled = false;
+			return $account->save();
 		}
 		return false;
 	}
 	
 	public function deleteAccount($container_guid){
-		if(check_entity_relationship($this->guid, 'accept_currency', $container_guid)){
-			return remove_entity_relationship($this->guid, 'accept_currency', $container_guid);
-		}
-		return false;
+		$account = new ElggLETSAccount($this->guid, $container_guid);
+		$account->disabled = true;
+		return true;//TODO
 	}
 	
 	public function hasAccount($container_guid){
 		$group = new ElggLETSGroup($container_guid);
-		return $group->isLETS()
-			&& $group->isMember($this->guid)
-			&& check_entity_relationship($this->guid, 'accept_currency', $group->guid);
-	}
-	
-	public function getAmount($container_guid){
-		if($this->hasAccount($container_guid)){
-			$account = new ElggLETSAccount($this->guid, $container_guid);
-			return $account->amount;
-		} else {
-			return 0;
-		}
+		return $group->isLETS() && $group->isMember($this->guid);
 	}
 }
