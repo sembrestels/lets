89d6899d56071ab82582fd112efb9b721a20a3b2
Sem <sembrestels@riseup.net>
2011-10-25 09:30:30 +0200

    Added owner menu.

--- start.php
+++ start.php
@@ -11,6 +11,9 @@ function lets_init() {
 	// routing of urls
 	elgg_register_page_handler('lets', 'lets_page_handler');
 	
+	// add LETS link to
+	elgg_register_plugin_hook_handler('register', 'menu:owner_block', 'lets_owner_block_menu');
+	
 	add_group_tool_option('lets', elgg_echo('lets:enablelets'), false);
 	elgg_extend_view('groups/tool_latest', 'lets/group_module');
 }
@@ -31,4 +34,23 @@ function lets_page_handler($page){
 	$body = elgg_view_layout('content', $params);
 
 	echo elgg_view_page($params['title'], $body);
+}
+
+/**
+ * Add a menu item to an ownerblock
+ */
+function lets_owner_block_menu($hook, $type, $return, $params) {
+	if (elgg_instanceof($params['entity'], 'user')) {
+		$url = "lets/owner/{$params['entity']->username}";
+		$item = new ElggMenuItem('lets', elgg_echo('lets'), $url);
+		$return[] = $item;
+	} else {
+		if ($params['entity']->lets_enable == "yes") {
+			$url = "lets/group/{$params['entity']->guid}/all";
+			$item = new ElggMenuItem('blog', elgg_echo('lets:group'), $url);
+			$return[] = $item;
+		}
+	}
+
+	return $return;
 }
