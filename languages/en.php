8974c42193be54ae9fc908a39dcfca9770f5110c
Sem <sembrestels@riseup.net>
2011-10-25 08:58:37 +0200

    Added group option.

---
+++ languages/en.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * LETS English language file.
+ *
+ */
+
+$english = array(
+	'lets' => 'Local Exchange Trading System',
+	'lets:enablelets' => 'Enable group currency',
+);
+
+add_translation('en', $english);
--- start.php
+++ start.php
@@ -7,5 +7,5 @@
 elgg_register_event_handler('init', 'system', 'lets_init');
 
 function lets_init() {
-	
+	add_group_tool_option('lets', elgg_echo('lets:enablelets'), false);
 }
