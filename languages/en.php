717165cfc58f4778d97bbfd37ed248978724f52c
Sem <sembrestels@riseup.net>
2011-10-28 14:21:33 +0200

    Added some translations.

--- languages/en.php
+++ languages/en.php
@@ -9,6 +9,10 @@ $english = array(
 	'lets:group' => 'Group economic system',
 	'lets:enablelets' => 'Enable group currency',
 	'lets:none' => 'Nobody is using it yet',
+	'lets:transfer' => 'Transfer',
+	'lets:account:delete' => 'Delete account',
+	'lets:concept' => 'Concept',
+	'lets:amount' => 'Amount',
 );
 
 add_translation('en', $english);
